9f83acf28c54cf455857c79b2947fe81df12a44a
Brahmprakash Gautam <brahmprakash.gautam03@gmail.com>
2024-06-17 00:11:05 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
     <>
     <Navbar/>
     <Home/>
-    {/* <Deliverables/>
-    <Products/> */}
-    {/* <Benefits />
-    <Contact/>*/}
+    <Deliverables/>
+    <Products/> 
+    <Benefits />
+    <Contact/>
     <Team/> 
     <Partners/>
     <Footer/>
